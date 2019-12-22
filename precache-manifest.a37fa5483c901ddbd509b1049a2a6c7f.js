ca310a996102a1908d3feb2aac5da979520aaec7
krylenger <krylenger@gmail.com>
2019-12-22 15:54:08 +0800

    Updates

---
+++ precache-manifest.a37fa5483c901ddbd509b1049a2a6c7f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "553d3009a4d70ee2b4626b0927f7c9ff",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "449890cabe2ea1118d2d",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "56135c37d31465af7b66",
+    "url": "/robofriends/static/css/main.791dcc0d.chunk.css"
+  },
+  {
+    "revision": "449890cabe2ea1118d2d",
+    "url": "/robofriends/static/js/2.f0d44eb8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.f0d44eb8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "56135c37d31465af7b66",
+    "url": "/robofriends/static/js/main.581b4636.chunk.js"
+  },
+  {
+    "revision": "04454491b22da9ad3e7d",
+    "url": "/robofriends/static/js/runtime-main.98228a54.js"
+  },
+  {
+    "revision": "7cc222a3be796abb86c6308b5862ff59",
+    "url": "/robofriends/static/media/Urba.7cc222a3.woff"
+  }
+]);
